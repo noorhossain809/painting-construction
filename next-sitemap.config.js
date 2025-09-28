e4d999fae26e7701a26f643535489cce356272e2
noorhossain809 <noorsan809@gmail.com>
2025-09-28 10:30:35 +0600

    adjustment-sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://propaintconstruction.com/', // domain
+  siteUrl: 'https://propaintconstruction.com', // domain
   generateRobotsTxt: true, // robots.txt o generate 
   sitemapSize: 7000,
 };
